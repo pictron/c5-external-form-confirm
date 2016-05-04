f69f1dbec19ffff5c972e181a20b4dd206af6572
pictron <sugi@pictron.net>
2016-05-04 21:42:15 +0900

    edit indent

--- controller/mailform.php
+++ controller/mailform.php
@@ -1,101 +1,101 @@
 <?php
-	namespace Application\Block\ExternalForm\Form\Controller;
-	use Concrete\Core\Controller\AbstractController;
-	use Core;
-	use UserInfo;
-	use Page;
-	class MailForm extends AbstractController {
+namespace Application\Block\ExternalForm\Form\Controller;
+use Concrete\Core\Controller\AbstractController;
+use Core;
+use UserInfo;
+use Page;
+class MailForm extends AbstractController {
+	
+	public function view(){
+		$this->set('bid', $this->bID);
+		$this->set('section', 'edit');
+		$this->set('errors', array());
+		$this->set('input', array());
+	}
+	
+	public function action_confirm() {
 		
-		public function view(){
-			$this->set('bid', $this->bID);
-			$this->set('section', 'edit');
-			$this->set('errors', array());
-			$this->set('input', array());
-		}
+		$input = $this->getpost($input);
 		
-		public function action_confirm() {
-			
-			$input = $this->getpost($input);
-			
-			if ($this->validate()) {
-                $section = 'confirm';
-				$this->set('input', $input);
-            } else {
-                $section = 'edit';
-            }
-            $this->set('section', $section);
-			$message = trim($this->post('message'));   		
-			$this->set('response', $message);  		
-			return true;    	
-		}
-		
-		public function action_send() {
-			
-			$input = $this->getpost($input);
-				
-			$mail = Core::make('helper/mail');
-			if ($this->validate()) {
-				$adminUserInfo = UserInfo::getByID(USER_SUPER_ID);
-                $formFormEmailAddress = $adminUserInfo->getUserEmail();
-				
-				$body = 'Name: ' . $input['name'] . "\r\n";
-	            $body .= 'Email: ' . $input['email'] . "\r\n\r\n";
-	            $body .= "Message: \r\n" . $input['message'];
-	            
-	            // Send the mail
-	            $mail->setSubject('問い合わせフォーム');
-	            $mail->setBody($body);
-	            $mail->to($formFormEmailAddress, 'admin');
-	            $mail->from($input['email'], $input['name']);
-	            $mail->replyto($input['email'], $input['name']);
-	            $mail->sendMail();
-	            $c = Page::getCurrentPage();
-		        header('location: '.Core::make('helper/navigation')->getLinkToCollection($c, true).'/complete');
-		        exit;
-			}
-		}
-		
-		public function action_complete() {
-            $this->set('section','complete');
+		if ($this->validate()) {
+            $section = 'confirm';
+			$this->set('input', $input);
+        } else {
+            $section = 'edit';
         }
+        $this->set('section', $section);
+		$message = trim($this->post('message'));   		
+		$this->set('response', $message);  		
+		return true;    	
+	}
+	
+	public function action_send() {
 		
-		public function action_back() {
-            $this->set('section','edit');
-        }
-        
-        private function validate() {
-	        $isvalid = true;
+		$input = $this->getpost($input);
 			
-			$data = $_POST;
-			$vf = Core::make('helper/validation/form');
-			$vf->setData($data);
-			$vf->addRequiredToken($this->bID.'ask');
-	        if(!$vf->test()){
-		        $isvalid = false;
-		    }
-		    
-		    $val = Core::make('helper/validation/strings');
-	        if (!strlen(trim($this->post('name')))) {
-		        $errors['name'] = '名前を入力してください';
-		        $isvalid = false;
-			}
-	        if(!$val->email($this->post('email'))){
-		        $errors['email'] = 'メールアドレスの形式を確認してください';
-		        $isvalid = false;
-	        }
-			if (!strlen(trim($this->post('name')))) {
-		        $errors['message'] = 'メッセージを入力してください';
-		        $isvalid = false;
-			}
-	        $this->set('errors', $errors);
-	        return $isvalid;
-	    }
-	    
-	    private function getpost($array) {
-		    $text = Core::make('helper/text');
-		    $array['name'] = $text->sanitize($this->post('name'));
-			$array['email'] = $text->sanitize($this->post('email'));
-			$array['message'] = $text->sanitize($this->post('message'));
-			return $array;
+		$mail = Core::make('helper/mail');
+		if ($this->validate()) {
+			$adminUserInfo = UserInfo::getByID(USER_SUPER_ID);
+            $formFormEmailAddress = $adminUserInfo->getUserEmail();
+			
+			$body = 'Name: ' . $input['name'] . "\r\n";
+            $body .= 'Email: ' . $input['email'] . "\r\n\r\n";
+            $body .= "Message: \r\n" . $input['message'];
+            
+            // Send the mail
+            $mail->setSubject('問い合わせフォーム');
+            $mail->setBody($body);
+            $mail->to($formFormEmailAddress, 'admin');
+            $mail->from($input['email'], $input['name']);
+            $mail->replyto($input['email'], $input['name']);
+            $mail->sendMail();
+            $c = Page::getCurrentPage();
+	        header('location: '.Core::make('helper/navigation')->getLinkToCollection($c, true).'/complete');
+	        exit;
 		}
 	}
+	
+	public function action_complete() {
+        $this->set('section','complete');
+    }
+	
+	public function action_back() {
+        $this->set('section','edit');
+    }
+    
+    private function validate() {
+        $isvalid = true;
+		
+		$data = $_POST;
+		$vf = Core::make('helper/validation/form');
+		$vf->setData($data);
+		$vf->addRequiredToken($this->bID.'ask');
+        if(!$vf->test()){
+	        $isvalid = false;
+	    }
+	    
+	    $val = Core::make('helper/validation/strings');
+        if (!strlen(trim($this->post('name')))) {
+	        $errors['name'] = '名前を入力してください';
+	        $isvalid = false;
+		}
+        if(!$val->email($this->post('email'))){
+	        $errors['email'] = 'メールアドレスの形式を確認してください';
+	        $isvalid = false;
+        }
+		if (!strlen(trim($this->post('name')))) {
+	        $errors['message'] = 'メッセージを入力してください';
+	        $isvalid = false;
+		}
+        $this->set('errors', $errors);
+        return $isvalid;
+    }
+    
+    private function getpost($array) {
+	    $text = Core::make('helper/text');
+	    $array['name'] = $text->sanitize($this->post('name'));
+		$array['email'] = $text->sanitize($this->post('email'));
+		$array['message'] = $text->sanitize($this->post('message'));
+		return $array;
+	}
+}
--- mailform.php
+++ mailform.php
@@ -1,67 +1,67 @@
 <?php
-	defined('C5_EXECUTE') or die("Access Denied.");
-	use Concrete\Core\Validation\CSRF\Token;
-	$form = Core::make('helper/form');
-	$val = Loader::helper('validation/form');
-	$token = Loader::helper('validation/token');
-	$crsftag = $token->generate($bid.'ask');
-	?>
-	<?php if ($section == 'edit') { ?>
-	<h2>お問い合わせ</h2>
-	<form method="post" action="<?php echo $view->action('confirm')?>">
-		<div class="form-group">
-		<?php echo $form->label('name', '名前'); ?>
-		<?php echo $form->text('name',$input['name'])?>
-		<?php echo(isset($errors['name']) ? '<font color="#ff0000">'.$errors['name'].'</font>' : ''); ?>
-		</div>
-		<div class="form-group">
-		<?php echo $form->label('email', 'メールアドレス'); ?>
-		<?php echo $form->text('email',$input['email'])?>
-		<?php echo(isset($errors['email']) ? '<font color="#ff0000">'.$errors['email'].'</font>' : ''); ?>
-		</div>
-		<div class="form-group">
-		<?php echo $form->label('message', 'メッセージ'); ?>
-		<?php echo $form->textarea('message',$input['message'])?>
-		<?php echo(isset($errors['message']) ? '<font color="#ff0000">'.$errors['message'].'</font>' : ''); ?>
-		</div>
-		<?php echo $form->hidden('ccm_token',$crsftag);?>
-		<?php echo $form->submit('submit','確認')?>
-	</form>
-	<?php } ?>
-	<?php if ($section == 'confirm'){ ?>
-	<h2>お問い合わせ：確認</h2>
-		<table class="table">
-			<tr>
-				<td>名前</td>
-				<td><?php echo $input['name']; ?></td>
-			</tr>
-			<tr>
-				<td>メールアドレス</td>
-				<td><?php echo $input['email']; ?></td>
-			</tr>
-			<tr>
-				<td>メッセージ</td>
-				<td><?php echo $input['message']; ?></td>
-			</tr>
-		</table>
-		<form method="post" id="form_confirm" action="<?php echo $view->action('send')?>">
-        <div class="form-group">
-            <?php echo $form->hidden('name')?>
-            <?php echo $form->hidden('email')?>
-            <?php echo $form->hidden('message')?>
-            <?php echo $form->hidden('ccm_token',$crsftag);?>
-            <?php echo $form->submit('submit','送信')?>
-            <a href="#" onclick="document.form_back.submit()" class="btn btn-default">戻る</a>
-        </div>
-    	</form>
-    	<form method="post" id="form_back" name="form_back" action="<?php echo $view->action('back')?>">
-	    	<?php echo $form->hidden('name')?>
-            <?php echo $form->hidden('email')?>
-            <?php echo $form->hidden('message')?>
-            <?php echo $form->hidden('ccm_token',$crsftag);?>
-	    </form>
-	<?php } ?>
-	<?php if ($section == 'complete'){ ?>
-	<h2>お問い合わせ：完了</h2>
-	<p>お問い合わせありがとうございました。</p>
-	<?php } ?>
+defined('C5_EXECUTE') or die("Access Denied.");
+use Concrete\Core\Validation\CSRF\Token;
+$form = Core::make('helper/form');
+$val = Loader::helper('validation/form');
+$token = Loader::helper('validation/token');
+$crsftag = $token->generate($bid.'ask');
+?>
+<?php if ($section == 'edit') { ?>
+<h2>お問い合わせ</h2>
+<form method="post" action="<?php echo $view->action('confirm')?>">
+	<div class="form-group">
+	<?php echo $form->label('name', '名前'); ?>
+	<?php echo $form->text('name',$input['name'])?>
+	<?php echo(isset($errors['name']) ? '<font color="#ff0000">'.$errors['name'].'</font>' : ''); ?>
+	</div>
+	<div class="form-group">
+	<?php echo $form->label('email', 'メールアドレス'); ?>
+	<?php echo $form->text('email',$input['email'])?>
+	<?php echo(isset($errors['email']) ? '<font color="#ff0000">'.$errors['email'].'</font>' : ''); ?>
+	</div>
+	<div class="form-group">
+	<?php echo $form->label('message', 'メッセージ'); ?>
+	<?php echo $form->textarea('message',$input['message'])?>
+	<?php echo(isset($errors['message']) ? '<font color="#ff0000">'.$errors['message'].'</font>' : ''); ?>
+	</div>
+	<?php echo $form->hidden('ccm_token',$crsftag);?>
+	<?php echo $form->submit('submit','確認')?>
+</form>
+<?php } ?>
+<?php if ($section == 'confirm'){ ?>
+<h2>お問い合わせ：確認</h2>
+<table class="table">
+	<tr>
+		<td>名前</td>
+		<td><?php echo $input['name']; ?></td>
+	</tr>
+	<tr>
+		<td>メールアドレス</td>
+		<td><?php echo $input['email']; ?></td>
+	</tr>
+	<tr>
+		<td>メッセージ</td>
+		<td><?php echo $input['message']; ?></td>
+	</tr>
+</table>
+<form method="post" id="form_confirm" action="<?php echo $view->action('send')?>">
+<div class="form-group">
+    <?php echo $form->hidden('name')?>
+    <?php echo $form->hidden('email')?>
+    <?php echo $form->hidden('message')?>
+    <?php echo $form->hidden('ccm_token',$crsftag);?>
+    <?php echo $form->submit('submit','送信')?>
+    <a href="#" onclick="document.form_back.submit()" class="btn btn-default">戻る</a>
+</div>
+</form>
+<form method="post" id="form_back" name="form_back" action="<?php echo $view->action('back')?>">
+	<?php echo $form->hidden('name')?>
+    <?php echo $form->hidden('email')?>
+    <?php echo $form->hidden('message')?>
+    <?php echo $form->hidden('ccm_token',$crsftag);?>
+</form>
+<?php } ?>
+<?php if ($section == 'complete'){ ?>
+<h2>お問い合わせ：完了</h2>
+<p>お問い合わせありがとうございました。</p>
+<?php } ?>
